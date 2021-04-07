97a611dcd652d8a70fb34ecc03fc4a0a0ba8e97e
jhadev <joshappeldev@gmail.com>
2021-04-07 08:37:29 -0500

    add

---
+++ yt-chat-bot.js
@@ -0,0 +1,9 @@
+function bot(message) {
+  const el = document.querySelector("div#input");
+  el.textContent = message;
+  el.dispatchEvent(new Event("input", { bubles: true, cancelable: true }));
+  document.querySelector('[aria-label="Send"]').click();
+}
+setInterval(() => {
+  bot("message goes here");
+}, 10000);
